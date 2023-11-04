e25935948313081879094516222a66586d17a652
vishnuxprasad <vshnprsdvs+github@gmail.com>
2023-11-04 11:42:28 +0530

    Basic configuration added.

--- ChatSql/ChatSql/settings.py
+++ ChatSql/ChatSql/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'speak2db',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'ChatSql.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
